cb1aee0f10183ae0b18cf61f1eef2220c7c0c2c8
JarrydLong <JarrydLong@users.noreply.github.com>
2020-09-24 23:00:44 +0200

    Core parts of courses and lessons
    Courses/Lessons core is complete
    Courses are protected by the required membership level
    Lessons are automatically protected by the courses chosen level
    Lessons can be overridden by showing to non members or a different level
    :goal_net: Basic progress tracking has been added
    You can mark a section of a course as complete
    Theres a progress bar at the top of the course

---
+++ js/admin.js
@@ -0,0 +1,88 @@
+
+function pmproc_editPost(post_id){
+	jQuery('#pmproc_post').val(post_id).trigger("change");
+	jQuery('#pmproc_save').html('Save');
+	location.href = "#pmproc_edit_post";
+}
+
+function pmproc_removePost(post_id) {
+	
+	var data = {
+		action: 'pmproc_remove_course',
+		course: pmpro_courses.course_id,
+		lesson: post_id
+	}
+
+	jQuery.ajax({
+		url: ajaxurl,
+		type:'POST',
+		timeout:2000,
+		dataType: 'html',
+		data: data,
+		error: function(xml){
+			alert('Error removing series post [1]');
+			//enable save button
+			jQuery('#pmproc_save').removeAttr('disabled');												
+		},
+		success: function(responseHTML){
+			if (responseHTML == 'error'){
+				alert('Error removing series post [2]');
+				//enable save button
+				jQuery('#pmproc_save').removeAttr('disabled');	
+			}else{
+				jQuery('#pmproc_table tbody').html(responseHTML);
+				// pmpros_Setup();
+			}																						
+		}
+	});
+}
+
+function pmproc_updatePost() {
+
+	jQuery(this).attr('disabled', 'true');	
+
+	var lesson_id = jQuery("#pmproc_post").val();
+
+	var data = {
+		action: 'pmproc_update_course',
+		course: pmpro_courses.course_id,
+		lesson: lesson_id
+	}
+	jQuery.ajax({
+		url: ajaxurl,
+		type: 'POST',
+		dataType: 'html',
+		data: data,
+		error: function(xml){
+			alert('Error saving series post [1]');
+			//enable save button
+			jQuery('#pmproc_save').html('Save');	
+			jQuery('#pmproc_save').removeAttr('disabled');												
+		},
+		success: function(responseHTML){
+			if (responseHTML == 'error'){
+				alert('Error saving series post [2]');
+				//enable save button
+				jQuery('#pmproc_save').html('Save');	
+				jQuery('#pmproc_save').removeAttr('disabled');		
+			}else{
+				jQuery('#pmproc_table tbody').html(responseHTML);
+				// pmpros_Setup();
+			}																						
+		}
+	});
+}
+
+function pmpros_Setup() {
+	jQuery('#pmproc_post').select2({width: 'elements'});
+	
+	jQuery('#pmproc_save').click(function() {
+		if( jQuery(this).attr('disabled') !== 'true' ){
+			pmproc_updatePost();
+		}
+	});
+}
+
+jQuery(document).ready(function(jQuery) {
+	pmpros_Setup();
+});
