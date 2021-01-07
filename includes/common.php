87f122f509457985446ef24baa5c56d2f8c6f399
Kim Coleman <kim@strangerstudios.com>
2021-01-06 20:58:01 -0500

    coding standards; function documentation

--- includes/common.php
+++ includes/common.php
@@ -1,26 +1,24 @@
 <?php
-
-function pmpro_courses_get_lessons( $course = 0 ){
-
+/**
+ * Get an array of lessons data assigned to this course ID.
+ *
+ */
+function pmpro_courses_get_lessons( $course = 0 ) {
 	global $wpdb;
-
 	$sql = "SELECT * FROM $wpdb->posts ";
-
-	if( $course !== 0 ){
+	if ( $course !== 0 ) {
 		$sql .= " LEFT JOIN $wpdb->postmeta on $wpdb->posts.ID = $wpdb->postmeta.post_id 
 		WHERE $wpdb->posts.post_type = 'pmpro_lesson' AND $wpdb->posts.post_status = 'publish'
 		AND $wpdb->postmeta.meta_key = 'pmproc_parent' AND $wpdb->postmeta.meta_value = '$course' ";
 	} else {
 		$sql .= "WHERE $wpdb->posts.post_type = 'pmpro_lesson' AND $wpdb->posts.post_status = 'publish' ";
 	}
-
 	$results = $wpdb->get_results( $sql );
 
+	// Build the array of $lessons.
 	$lessons = array();
-
-	if( !empty( $results ) ){
-
-		foreach( $results as $result ){
+	if ( ! empty( $results ) ) {
+		foreach( $results as $result ) {
 			$lessons[] = array(
 				'id' 	=> $result->ID,
 				'title' => $result->post_title,
@@ -28,32 +26,24 @@ function pmpro_courses_get_lessons( $course = 0 ){
 				'excerpt' => $result->post_excerpt,
 				'permalink' => get_the_permalink( $result->ID )
 			);
-
 		}
-
 	}
-	
 	return $lessons;
-
 }
 
-function pmpro_courses_get_course( $lesson_id ){
-
+/**
+ * Get a link to the course assigned to this lesson ID.
+ *
+ */
+function pmpro_courses_get_course( $lesson_id ) {
 	$parent = intval( get_post_meta( $lesson_id, 'pmproc_parent', true ) );
-
-	if( $parent ){
-
+	if ( $parent ) {
 		$course = get_post( $parent );
-
-		if( $course ){
-
-			return "<a href='".admin_url( 'post.php?post='.$parent.'&action=edit' )."'>".$course->post_title."</a>";
-
+		if ( $course ) {
+			return '<a href="' . esc_url( add_query_arg( array( 'post' => $parent, 'action' => 'edit' ), admin_url( 'post.php' ) ) ) . '">' . esc_html( $course->post_title ) . '</a>';
 		}
 	}
-
-	return __('Lesson Not Assigned', 'pmpro-courses');
-
+	return '&#8212;';
 }
 
 function pmpro_courses_build_lesson_html( $array_content ){
@@ -85,20 +75,18 @@ function pmpro_courses_build_lesson_html( $array_content ){
 
 }
 
-function pmpro_courses_get_lesson_count( $course ){
-
+/**
+ * Get a count of lessons assigned to this course ID.
+ *
+ */
+function pmpro_courses_get_lesson_count( $course_id ) {
 	global $wpdb;
-
 	$sql = "SELECT count(*) FROM $wpdb->posts ";
-
 	$sql .= " LEFT JOIN $wpdb->postmeta on $wpdb->posts.ID = $wpdb->postmeta.post_id 
 		WHERE $wpdb->posts.post_type = 'pmpro_lesson' AND $wpdb->posts.post_status = 'publish'
-		AND $wpdb->postmeta.meta_key = 'pmproc_parent' AND $wpdb->postmeta.meta_value = '$course' ";
-
+		AND $wpdb->postmeta.meta_key = 'pmproc_parent' AND $wpdb->postmeta.meta_value = '$course_id' ";
 	$results = $wpdb->get_var( $sql );
-
 	return $results;
-
 }
 
 function pmpro_courses_check_level( $post_id ){
@@ -158,42 +146,38 @@ function pmpro_courses_check_level( $post_id ){
 
 }
 
-function pmproc_complete_button( $lid, $cid ){
-
+/**
+ * Display a button that allows a user to mark a course as complete.
+ *
+ */
+function pmproc_complete_button( $lid, $cid ) {
 	$button_text = apply_filters( 'pmproc_button_to_complete_text', __('Mark As Complete', 'pmpro-courses') );
-
 	$complete_text = apply_filters( 'pmproc_button_complete_text', __('Completed', 'pmpro-courses' ) );
 
-	$content = "<span><button class='pmproc_button_mark_complete_action ".pmpro_get_element_class('span_pmpro_checkout_button pmproc_button_mark_complete')."' lid='".$lid."' cid='".$cid."'>".$button_text."</button></span>";
+	$content = '<button class="pmproc_button_mark_complete_action" lid="' . $lid . '" cid="' . $cid . '">' . esc_html( $button_text ) . '</button>';
 
 	$user = wp_get_current_user();
-
-	if( $user ){
-
+	if ( ! empty( $user ) && ! empty( $user->ID ) ) {
 		$user_id = $user->ID;
-
-		$progress = get_user_meta( $user_id, 'pmproc_progress_'.$cid, true );
-
-		if( !empty( $progress ) ){
-
-			if( in_array( $lid, $progress ) ){
-
+		$progress = get_user_meta( $user->ID, 'pmproc_progress_' . $cid, true );
+		if ( ! empty( $progress ) ) {
+			if ( in_array( $lid, $progress ) ) {
 				$show_complete_button = apply_filters( 'pmproc_button_show_complete', true );
-
-				if( $show_complete_button ){
-				
-					$content = "<span><button class='".pmpro_get_element_class('span_pmpro_checkout_button pmproc_button_mark_complete')."'>".$complete_text."</button></span>";
+				if ( $show_complete_button ) {
+					$content = '<button class="pmproc_button_mark_complete">'. esc_html( $complete_text ) . '</button>';
 				}
 			}
 		}
 	}
 
 	return $content;
-
 }
 
-function pmproc_get_next_lesson( $lid, $cid ){
-
+/**
+ * Get the permalink for the next lesson in the course.
+ *
+ */
+function pmproc_get_next_lesson( $lid, $cid ) {
 	$args = array(
 		'post_type' => 'pmpro_lesson', 
 		'posts_per_page' => -1,
@@ -205,12 +189,10 @@ function pmproc_get_next_lesson( $lid, $cid ){
 			)
 		)
 	);
-
 	$the_query = new WP_Query( $args );
 
 	$lessons = array();
-
-	if( $the_query->have_posts() ){
+	if ( $the_query->have_posts() ){
 		while( $the_query->have_posts() ){
 			$the_query->the_post();
 			$lessons[] = get_the_ID();
@@ -223,44 +205,37 @@ function pmproc_get_next_lesson( $lid, $cid ){
 
 	$next_key = $searched + 1;
 
-	if( isset( $lessons[$searched] ) ){
+	if ( isset( $lessons[$searched] ) ) {
 		return get_the_permalink( $lessons[$next_key] );
 	} else {
 		return get_the_permalink( $lessons[$searched] );
 	}
-
 }
 
-function pmproc_get_user_progress( $course_id ){
-
+/**
+ * Get the user's course progress expressed as a percentage of lessons completed.
+ *
+ */
+function pmproc_get_user_progress( $course_id ) {
 	$percentage = 0;
-
 	$lesson_count = pmpro_courses_get_lesson_count( $course_id );
-
 	$progress = count( pmproc_get_complete_lessons( $course_id ) );
-
-	if( $lesson_count !== 0 && $progress !== 0 ){
-	
+	if ( $lesson_count !== 0 && $progress !== 0 ) {
 		$percentage = $progress / $lesson_count * 100;
-
 	}
-
 	return $percentage;
-
 }
 
+/**
+ * Display a progress bar for the current user and course ID.
+ *
+ */
 function pmproc_display_progress_bar( $course_id ){
-
 	$percentage = pmproc_get_user_progress( $course_id );
-
-	if( $percentage !== 0 ){
-
-		return '<div><div data-preset="line" class="ldBar" data-value="'.$percentage.'" style="width: 	100%;"></div></div>';
-
+	if ( $percentage !== 0 ) {
+		return '<div><div data-preset="line" class="ldBar" data-value="' . $percentage . '" style="width: 	100%;"></div></div>';
 	} 
-
 	return;
-
 }
 
 function pmproc_get_complete_lessons( $cid ){
