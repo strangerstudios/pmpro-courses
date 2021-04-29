814a3d0a96b14c99a1a9c887a30da647f9aa931e
Jason Coleman <jason@strangerstudios.com>
2021-04-30 04:31:46 +1000

    Non-course CPTs will now check for access to associated course. Now redirecting away from lessons/etc when you don't have access.

--- includes/modules/learndash.php
+++ includes/modules/learndash.php
@@ -16,8 +16,9 @@ class PMPro_Courses_LearnDash extends PMPro_Courses_Module {
      */
     public function init_active() {
         add_action('admin_menu', array( 'PMPro_Courses_LearnDash', 'admin_menu' ), 20);
+		add_filter( 'pmpro_has_membership_access_filter', array( 'PMPro_Courses_LearnDash', 'pmpro_has_membership_access_filter' ), 10, 4 );
 		add_action( 'template_redirect', array( 'PMPro_Courses_LearnDash', 'template_redirect' ) );
-        add_filter( 'pmpro_membership_content_filter', array( 'PMPro_Courses_LearnDash', 'pmpro_membership_content_filter' ), 10, 2 );
+        add_filter( 'pmpro_membership_content_filter', array( 'PMPro_Courses_LearnDash', 'pmpro_membership_content_filter' ), 10, 2 );		
     }
 	
 	/**
@@ -42,6 +43,36 @@ class PMPro_Courses_LearnDash extends PMPro_Courses_Module {
 	}
 	
 	/**
+	 * Filter PMPro access so check on lessons and
+	 * other LD post types checks the associated course.
+	 */
+	public static function pmpro_has_membership_access_filter( $hasaccess, $mypost, $myuser, $post_membership_levels ) {
+		// Don't need to check if already restricted.
+		if ( !  $hasaccess ) {
+			return $hasaccess;
+		}
+
+		// Don't need to check unless it's an LD non-course CPT
+		$ld_non_course_cpts = array( 'sfwd-lessons', 'sfwd-topic', 'sfwd-quiz', 'sfwd-question', 'sfwd-certificates', 'groups', 'sfwd-assignment' );					
+		if ( ! in_array( $mypost->post_type, $ld_non_course_cpts ) ) {
+			return $hasaccess;
+		}
+		
+		// Let admins in.
+		if ( current_user_can( 'manage_options' ) ) {
+			return $hasaccess;
+		}
+		
+		// Okay check for an associated course_id.
+		$course_id = get_post_meta( $mypost->ID, 'course_id', true );
+		if ( ! empty( $course_id ) ) {
+			$hasaccess = pmpro_has_membership_access( $course_id, $myuser->ID );
+		}
+		
+		return $hasaccess;
+	}
+	
+	/**
 	 * If a course requires membership, redirect its lessons
 	 * to the main course page.
 	 */
@@ -58,12 +89,16 @@ class PMPro_Courses_LearnDash extends PMPro_Courses_Module {
 			// Let admins in.
 			if ( current_user_can( 'manage_options' ) ) {
 				return;
-			}		
+			}
 			
 			// Okay check access.
-			// TODO handle non courses. Check the related course. Maybe filter pmpro_has_membership_access instead of adding logic here.
-			$access = pmpro_has_membership_access( $post->ID );
-			
+			$course_id = get_post_meta( $post->ID, 'course_id', true );			
+			if ( $post->post_type != 'sfwd-courses' && ! empty( $course_id ) ) {
+				$access = pmpro_has_membership_access( $course_id );
+			} else {
+				$access = pmpro_has_membership_access( $post->ID );
+			}
+
 			// They have access. Let em in.
 			if ( $access ) {
 				return;
@@ -80,7 +115,12 @@ class PMPro_Courses_LearnDash extends PMPro_Courses_Module {
 				$redirect_to = apply_filters( 'pmpro_courses_course_redirect_to', null );				
 			} else {
 				// Send lessons and other content to the parent course.
-				$redirect_to = apply_filters( 'pmpro_courses_lesson_redirect_to', get_permalink( $post->post_parent ) );
+				if ( ! empty( $course_id ) ) {
+					$redirect_to = get_permalink( $course_id );
+				} else {
+					$redirect_to = null;
+				}
+				$redirect_to = apply_filters( 'pmpro_courses_lesson_redirect_to', $redirect_to );
 			}
 			
 			if ( $redirect_to ) {
