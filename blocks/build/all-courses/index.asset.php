19d6e9a7ae7885030ccf457e1fe1756d1f194056
Andrew Lima <andrew@arctek.co.za>
2022-09-26 20:29:08 +0200

    Change icons + run build

--- blocks/build/all-courses/index.asset.php
+++ blocks/build/all-courses/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3db1b18c995e319370f6');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6d336290795ef7b3f0b2');
--- blocks/build/my-courses/index.asset.php
+++ blocks/build/my-courses/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f9161c757bd85094b793');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '82946884d4abad23d693');
