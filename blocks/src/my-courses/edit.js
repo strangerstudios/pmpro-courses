c35cd5cab002ad267358f7afd85b233015d5bf26
David Wanjuki <wanjuki@outlook.com>
2025-11-27 18:57:45 +0300

    Add fragment and props

--- blocks/src/my-courses/edit.js
+++ blocks/src/my-courses/edit.js
@@ -9,22 +9,26 @@ function Edit({ attributes, setAttributes }) {
     const { limit } = attributes
     const blockProps = useBlockProps({ className: "pmpro-block-element"});
 
-    return [
-        <InspectorControls>
-            <PanelBody>
-                <TextControl
-                    label={__('Limit', 'pmpro-courses')}
-                    value={limit}
-                    onChange={(newLimit) => setAttributes({ limit: newLimit } ) }
-                    help={__('How many courses should be displayed. Set the value to -1 to show all courses.', 'pmpro-courses')}
-            />
-            </PanelBody>
-        </InspectorControls>,
-        <div {...blockProps}>
-            <span className="pmpro-block-title">{__('Paid Memberships Pro Courses', 'pmpro-courses')}</span>
-            <span className="pmpro-block-subtitle">{__('My Courses', 'pmpro-courses')}</span>
-        </div>
-    ];
+    return (
+        <>
+            <InspectorControls>
+                <PanelBody>
+                    <TextControl
+                        __next40pxDefaultSize
+                        __nextHasNoMarginBottom
+                        label={__('Limit', 'pmpro-courses')}
+                        value={limit}
+                        onChange={(newLimit) => setAttributes({ limit: newLimit } ) }
+                        help={__('How many courses should be displayed. Set the value to -1 to show all courses.', 'pmpro-courses')}
+                />
+                </PanelBody>
+            </InspectorControls>
+            <div {...blockProps}>
+                <span className="pmpro-block-title">{__('Paid Memberships Pro Courses', 'pmpro-courses')}</span>
+                <span className="pmpro-block-subtitle">{__('My Courses', 'pmpro-courses')}</span>
+            </div>
+        </>
+    );
 }
 
 export default Edit;
