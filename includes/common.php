6aa2f7fc8fcbdf302584ff3d1476a81c7f6e52c4
Andrew Lima <andrew@arctek.co.za>
2026-05-26 12:46:44 +0200

    Fix issue with helper function
    Resolves: https://github.com/strangerstudios/pmpro-courses/issues/121

--- includes/common.php
+++ includes/common.php
@@ -589,13 +589,13 @@ function pmpro_courses_get_post_ids_from_levels( $level_ids, $post_type ) {
 	$sql = $wpdb->prepare(
 		"SELECT mp.page_id
 		FROM {$wpdb->pmpro_memberships_pages} mp
-		LEFT JOIN {$wpdb->posts} p ON mp.page_id = p.ID
+		INNER JOIN {$wpdb->posts} p ON mp.page_id = p.ID
 		WHERE mp.membership_id IN ({$levels})
 		AND p.post_type = %s
 		AND p.post_status = 'publish'
 		GROUP BY mp.page_id",
-		$post_type
+		array_merge( $level_ids, array( $post_type ) )
 	);
 
-	return $wpdb->get_col( $sql );
+	return $wpdb->get_col( $sql ) ?: array();
 }
